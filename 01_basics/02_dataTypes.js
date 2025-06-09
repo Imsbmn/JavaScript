016c6d7633b9cbafbc2ab374d2cb555be5dacbcd
Shubham Kumar Ray <150553890+Imsbmn@users.noreply.github.com>
2025-06-09 19:12:27 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert("3 + 5") // we are using node.js , not Browser
+
+console.log(5+
+    5); // code readability should be high
+
+console.log("Shubham")
+
+
+let name = "Shubham"
+let age = 22
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Shubham");
+
+console.log(typeof age);
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
