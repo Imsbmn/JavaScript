3dd0ad6524f72b937a9a9b5aa1c473ab66529639
Shubham Kumar Ray <150553890+Imsbmn@users.noreply.github.com>
2025-06-14 06:04:33 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log("5" > 3);
+// console.log("05" > 3);    // 2 alag alag DataType ka comparison nhi kiya ja sakta , yaha par javaScript number me change kar de raha hai
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);    // undefined kisi bhi case me comparison karne par false dega
+console.log(undefined >= 0);
+console.log(undefined <=! 0);
+
+//  ===     value ke sath sath dataType ko bhi check karta hai
+
+console.log("5" === 5);
