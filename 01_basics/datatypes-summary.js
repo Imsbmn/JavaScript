1f6bc6cdb4a71961ccf7e2714b1b3db3ef02e96b
Shubham Kumar Ray <150553890+Imsbmn@users.noreply.github.com>
2025-06-15 11:24:00 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-// Primitive datatype     // call by value hote hai matlab jab bhi hum inko kahi se kahi copy karte hai to inka jo original data hai wo reference memory ko nhi diya jata inko  copy kar ke diya jata hai alag aur jo bhi hum unme changes karte hai wo copy me changes karte hai 
+//  Primitive datatype     // call by value hote hai matlab jab bhi hum inko kahi se kahi copy karte hai to inka jo original data hai wo reference memory ko nhi diya jata inko  copy kar ke diya jata hai alag aur jo bhi hum unme changes karte hai wo copy me changes karte hai 
 
 //  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
@@ -21,9 +21,9 @@ console.log(id === anotherId);
 
 
 
-// Non-Primitive datatype (Reference type bhi bolte hai )
+//  Non-Primitive datatype (Reference type bhi bolte hai )
 
-// Array, Objects, Functions
+//  Array, Objects, Functions
 
 
 const heroes = ["Shaktiman", "Balveer", "doga"];     //  Array 
@@ -46,6 +46,57 @@ console.log(typeof scoreValue);
 console.log(typeof outsideideTemp);
 console.log(typeof myFunction);
 console.log(typeof heroes);
+
+
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+
+// Memory (stack memory aur Heap memory)
+
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// Stack : jab bhi stack memory use hoti hai matlab jo bhi variable declare kiya uska hume 1 copy milta hai
+// Heap : jab bhi koi 1 memory heap ke andar defined hoti hai jaise koi object ya kuch bhi to heap ke andar chala jata hai to waha se milta hai Reference original value ka to jo bhi change karenge wo original value me change me hoga
+
+
+
+let myName = "Shubham"
+let anotherName = "myName"
+anotherName = "Kumar"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    Email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "shubh@gmail.com"
+
+console.log(userOne.Email);
+console.log(userTwo.Email);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
